203b891398583a675c59af864afe24e97a970064
ahmadswalih <ahmadswalih777@gmail.com>
2024-04-22 22:21:02 +0530

    updated the quotesCard and added the functionality for the application

--- app/components/QuotesCard.jsx
+++ app/components/QuotesCard.jsx
@@ -1,8 +1,10 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoCopyOutline, IoLogoInstagram, IoRefresh } from "react-icons/io5"; // Replace with your actual icon library
+import quotes from "../data/quotes.json";
 
-const MondayQuoteCard = ({ quote }) => {
+const MondayQuoteCard = () => {
+  const [quote, setQuote] = useState("");
   const [copied, setCopied] = useState(false);
 
   const copyToClipboard = () => {
@@ -17,18 +19,20 @@ const MondayQuoteCard = ({ quote }) => {
     // Implement your share functionality here
   };
 
+  // Function to fetch a random quote from the JSON data
   const generateNewQuote = () => {
-    // Implement your new quote generation logic here
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const newQuote = quotes[randomIndex].quote;
+    setQuote(newQuote);
   };
-
+  useEffect(() => {
+    generateNewQuote();
+  }, []);
   return (
-    <div className="bg-white mt-24 dark:bg-gray-800 max-w-md mx-auto rounded-lg shadow-lg overflow-hidden">
+    <div className="bg-white mt-24 z-50 dark:bg-gray-800 max-w-md mx-auto rounded-lg shadow-lg overflow-hidden">
       <div className="p-6">
         <div className="text-lg font-semibold text-gray-800 dark:text-white mb-4">
-          <p>
-            Code your dreams into reality, one line at a time. Let's crush it
-            this week!
-          </p>
+          <p>{quote}</p>
         </div>
         <div className="flex items-center justify-between">
           <button
@@ -45,7 +49,7 @@ const MondayQuoteCard = ({ quote }) => {
             <IoLogoInstagram className="w-5 h-5" />
           </button>
           <button
-            className="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-300 focus:outline-none"
+            className="text-gray-600  cursor-pointer hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-300 focus:outline-none"
             onClick={generateNewQuote}
           >
             <IoRefresh className="w-5 h-5" />
