d974d32f6e70c6fbd54bbc6b4e31e528a9b8ce0c
ahmadswalih <ahmadswalih777@gmail.com>
2024-04-23 20:42:27 +0530

    added google analytics

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Reenie+Beanie&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q1QP8RE5WX"
+      ></script>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q1QP8RE5WX"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-Q1QP8RE5WX');
+      </script>
       <body className={inter.className}>{children}</body>
     </html>
   );
